0c46b50d9face035e633bf738ed977b414347b78
CrazyProger1 <crazyproger1@gmail.com>
2023-06-09 13:13:22 +0300

    Fix: default values

--- server/abandoned/models.py
+++ server/abandoned/models.py
@@ -1,26 +1,9 @@
 import cities_light
-from django.core import validators
 from django.db import models
 from abandoned.limits import *
 
 
 # from django.contrib.gis.db import models
-
-
-class AbandonedObjectState(models.TextChoices):
-    DANGEROUS = 'd', 'dangerous'
-    BAD = 'b', 'bad'
-    AVERAGE = 'a', 'average'
-    GOOD = 'g', 'good'
-    NOVELTY = 'n', 'novelty'
-
-
-class AbandonedObjectCategory(models.Model):
-    class Meta:
-        verbose_name = 'AbandonedObjectCategory'
-        verbose_name_plural = 'AbandonedObjectCategories'
-
-    name = models.CharField(max_length=CATEGORY_NAME_LENGTH, primary_key=True)
 
 
 class Coordinates(models.Model):
@@ -50,11 +33,27 @@ class Address(models.Model):
         verbose_name_plural = 'Addresses'
 
     id = models.AutoField(primary_key=True)
-    country = models.ForeignKey('cities_light.Country', on_delete=models.CASCADE, null=False)
-    city = models.ForeignKey('cities_light.City', on_delete=models.CASCADE, null=False)
-    street = models.CharField(max_length=STREET_NAME_LENGTH, null=True, default=None)
-    street_number = models.CharField(max_length=4, null=True, default=None)
-    zipcode = models.CharField(max_length=10, null=True, default=None)
+    country = models.ForeignKey('cities_light.Country', on_delete=models.CASCADE)
+    city = models.ForeignKey('cities_light.City', on_delete=models.CASCADE)
+    street = models.CharField(max_length=STREET_NAME_LENGTH, blank=True, null=True)
+    street_number = models.CharField(max_length=4, blank=True, null=True)
+    zipcode = models.CharField(max_length=10, blank=True, null=True)
+
+
+class AbandonedObjectState(models.TextChoices):
+    DANGEROUS = 'd', 'dangerous'
+    BAD = 'b', 'bad'
+    AVERAGE = 'a', 'average'
+    GOOD = 'g', 'good'
+    NOVELTY = 'n', 'novelty'
+
+
+class AbandonedObjectCategory(models.Model):
+    class Meta:
+        verbose_name = 'AbandonedObjectCategory'
+        verbose_name_plural = 'AbandonedObjectCategories'
+
+    name = models.CharField(max_length=CATEGORY_NAME_LENGTH, primary_key=True)
 
 
 class AbandonedObjectLocation(models.Model):
@@ -80,8 +79,8 @@ class AbandonedObject(models.Model):
         verbose_name_plural = 'AbandonedObjects'
 
     id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=ABANDONED_OBJECT_NAME_LENGTH, null=False)
-    description = models.CharField(max_length=ABANDONED_OBJECT_DESC_LENGTH, null=False)
+    name = models.CharField(max_length=ABANDONED_OBJECT_NAME_LENGTH)
+    description = models.CharField(max_length=ABANDONED_OBJECT_DESC_LENGTH)
     hidden = models.BooleanField(default=False)
     category = models.ForeignKey(
         to=AbandonedObjectCategory,
@@ -92,10 +91,8 @@ class AbandonedObject(models.Model):
         max_length=1,
         choices=AbandonedObjectState.choices,
         default=AbandonedObjectState.AVERAGE,
-        null=False
     )
     location = models.ForeignKey(
         to=AbandonedObjectLocation,
         on_delete=models.CASCADE,
-        null=False
     )
